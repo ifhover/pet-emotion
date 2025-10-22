8aeac5042bf092cfc571b36a397357778dd0bf04
sudden3 <ifhover@gmail.com>
2025-10-22 09:31:34 +0800

    update pm2 config:instances

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'pet-server',
       script: 'dist/src/main.js',
-      instances: 'max',
+      instances: 1,
       exec_mode: 'cluster',
       env: {
         PORT: 7000,
